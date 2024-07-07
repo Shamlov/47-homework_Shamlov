7e32fd5651f183ca02c851ef7773169b177b1fe2
Shamlov <pshamlov@gmail.com>
2024-07-07 09:41:44 +0300

    настроить регулярки

--- js/script.js
+++ js/script.js
@@ -9,10 +9,8 @@ let colorArr = []    // храним массив всех цветов
 btn.addEventListener('click', btnclick ) // клик по кнопке
 function btnclick() {
     formVerification()
+    
 }
-
-
-
 
 function formVerification() {    // проверка формы и запись в массив . не могу составить регулярки в проверках
     let data = {}        // получаем данные из формы
@@ -34,20 +32,20 @@ function formVerification() {    // проверка формы и запись 
         document.querySelector('#colorNameLabel').classList.add('invalid-field')
     }
     /////////проверяем формат и введеные значения формата///////////////////////////////
-    if(type.value == 'RGB' && /\d/gi.test(`${code.value}`)) {     // тут проблема с регуляркой . не могу составить. пишу только цифры
-        data.type ='RGB'
+    if(type.value == 'rgb' && /./gi.test(`${code.value}`)) {     // тут проблема с регуляркой . не могу составить. пишу только цифры
+        data.type ='rgb'
         data.code = `${code.value}`
         status2 = true
     }
 
-    if(type.value == 'RGBA' && /\d/gi.test(`${code.value}`)) {     // тут проблема с регуляркой . не могу составить. пишу только цифры
-        data.type ='RGBA'
+    if(type.value == 'rgba' && /./gi.test(`${code.value}`)) {     // тут проблема с регуляркой . не могу составить. пишу только цифры
+        data.type ='rgba'
         data.code = `${code.value}`
         status2 = true
     }
 
-    if(type.value == 'HEX' && /\d[a-f]/gi.test(`${code.value}`)) {     // тут проблема с регуляркой . не могу составить. пишу только цифры
-        data.type ='HEX'
+    if(type.value == 'hex' && /./gi.test(`${code.value}`)) {     // тут проблема с регуляркой . не могу составить. пишу только цифры
+        data.type ='hex'
         data.code = `${code.value}`
         status2 = true
     } else 
@@ -59,10 +57,36 @@ function formVerification() {    // проверка формы и запись 
     }
 
     if(status2 && status1) {    // если оба условия выполнены . запишем в массив
-        console.log(1234567)
         colorArr.push(data)
         colorName.value = ''
         code.value = ''
+        addingСard()
     }
-    console.log(colorArr)
 }
+
+let paletteBlock = document.querySelector('#paletteBlock')
+
+function addingСard() {
+    let div = document.createElement('div') 
+    div.className = 'color-cards'
+    let a = colorArr.slice(-1)[0].type
+    let b = ((colorArr.slice(-1)[0].code))
+    if(colorArr.slice(-1)[0].type == 'hex') {
+        a = ''
+        b = colorArr.slice(-1)[0].code
+        div.style.cssText = `background-color: ${a} ${b}`
+    } else {
+        div.style.cssText = `background-color: ${a}(${b})`
+    }
+    paletteBlock.prepend(div)
+    div.innerHTML  = `
+    <div class="color-card">
+        <div class="card-name">
+            <p>${colorArr.slice(-1)[0].colorName}</p>
+            <p>${colorArr.slice(-1)[0].type}</p>
+            <p>${colorArr.slice(-1)[0].code}</p>
+        </div>
+    </div>
+    `
+}
+
