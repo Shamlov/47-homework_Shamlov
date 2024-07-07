177beb8fc90668af602417aab57a9ac90382d551
Shamlov <pshamlov@gmail.com>
2024-07-07 11:21:15 +0300

    итог

--- js/script.js
+++ js/script.js
@@ -2,9 +2,7 @@ const btn = document.querySelector('button')
 const colorName = document.querySelector('#colorName')
 const type = document.querySelector('#type')
 const code = document.querySelector('#code')
-
 let colorArr = []    // храним массив всех цветов   
-
 
 btn.addEventListener('click', btnclick ) // клик по кнопке
 function btnclick() {
@@ -12,7 +10,8 @@ function btnclick() {
     
 }
 
-function formVerification() {    // проверка формы и запись в массив . не могу составить регулярки в проверках
+function formVerification() {    // проверка формы и запись в массив.
+    // не реализована проверка диапазона введенных значений 
     let data = {}        // получаем данные из формы
     let status1 = false
     let status2 = false
@@ -23,7 +22,6 @@ function formVerification() {    // проверка формы и запись 
             nameMatchBoolean = false
         }
     })
-    // console.log(/\D/gi.test(`${colorName.value}`))   //тут верно если одновременно и буквы и циыры. но как сделать ложным если встечается хоть одна цифра
     if(/^\D/gi.test(`${colorName.value}`) && nameMatchBoolean) {
         document.querySelector('#colorNameLabel').classList.remove('invalid-field')
         data.colorName = `${colorName.value}`
@@ -32,24 +30,21 @@ function formVerification() {    // проверка формы и запись 
         document.querySelector('#colorNameLabel').classList.add('invalid-field')
     }
     /////////проверяем формат и введеные значения формата///////////////////////////////
-    if(type.value == 'rgb' && /./gi.test(`${code.value}`)) {     // тут проблема с регуляркой . не могу составить. пишу только цифры
+    if(type.value == 'rgb' && /^(\d{1,3}),(\d{1,3}),(\d{1,3})$/gi.test(`${code.value}`)) {     // тут проблема с регуляркой . не могу составить. пишу только цифры
         data.type ='rgb'
         data.code = `${code.value}`
         status2 = true
     }
-
-    if(type.value == 'rgba' && /./gi.test(`${code.value}`)) {     // тут проблема с регуляркой . не могу составить. пишу только цифры
+    if(type.value == 'rgba' && /^(\d{1,3}),(\d{1,3}),(\d{1,3}),(\d\.?\d?)$/gi.test(`${code.value}`)) {     // тут проблема с регуляркой . не могу составить. пишу только цифры
         data.type ='rgba'
         data.code = `${code.value}`
         status2 = true
     }
-
-    if(type.value == 'hex' && /./gi.test(`${code.value}`)) {     // тут проблема с регуляркой . не могу составить. пишу только цифры
+    if(type.value == 'hex' && /^#([0-9a-fA-F]{3}){1,2}$/gi.test(`${code.value}`)) {     // тут проблема с регуляркой . не могу составить. пишу только цифры
         data.type ='hex'
         data.code = `${code.value}`
         status2 = true
     } else 
-
     if(status2) {
         document.querySelector('#codeLabel').classList.remove('invalid-field')
     } else {
@@ -65,7 +60,6 @@ function formVerification() {    // проверка формы и запись 
 }
 
 let paletteBlock = document.querySelector('#paletteBlock')
-
 function addingСard() {
     let div = document.createElement('div') 
     div.className = 'color-cards'
